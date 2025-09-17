75268634c8003b7e0970e406d2815b0348f0df84
donia225 <donia.drira@esprit.tn>
2025-09-17 18:47:08 +0100

    prometheus

--- backend/GestionRh/gestion_rh/gestion_rh/settings.py
+++ backend/GestionRh/gestion_rh/gestion_rh/settings.py
@@ -32,7 +32,12 @@ SECRET_KEY = 'django-insecure-_+a^@3%@%(e+&b!y=vo1bb!!v+mjb&_g)rt*mpj*yi1nc9fv4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1", "localhost",
+    "192.168.1.172",          
+    "gestionrh-backend",    
+]
+
 
 AUTH_USER_MODEL = 'recrutement.User'
 # Application definition
