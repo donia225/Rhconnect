1a7711e3147e017a5cfa07002691f5658d7b573b
donia225 <donia.drira@esprit.tn>
2025-09-30 00:46:51 +0100

    some changes

--- frontend/Rhconnect/src/environments/environment.prod.ts
+++ frontend/Rhconnect/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   production: true,
-  apiUrl: 'http://34.42.155.127/api/',
+  apiUrl: 'http://34.42.155.127/api',
   appVersion: packageInfo.version
 };
